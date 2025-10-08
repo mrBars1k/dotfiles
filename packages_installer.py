c19b7bb99c31e42dbb4766b25d0559492bb69e61
mrBars1k <mrBars1k@proton.me>
2025-10-08 20:38:16 +0300

    [upd] xdg-desktop-portal-hyprland

--- packages_installer.py
+++ packages_installer.py
@@ -5,8 +5,8 @@ import re
 
 
 PACMAN_PACKAGES = [
-    "hyprland", "hyprshot", "hyprsunset", "hyprpaper", "waybar", "wofi", "swaync", "swappy", "networkmanager",
-    "kitty", "alacritty", "ranger", "ueberzugpp", "fastfetch", "neovim", "zsh", "sddm",
+    "hyprland", "hyprshot", "hyprsunset", "hyprpaper", xdg-desktop-portal-hyprland, "waybar", "wofi", "swaync", "swappy",
+    "kitty", "alacritty", "ranger", "ueberzugpp", "fastfetch", "neovim", "zsh", "sddm", "networkmanager",
     "duf", "dysk", "os-prober", "ntfs-3g", "syncthing", "obsidian", "flatpak",
     "pipewire", "pipewire-alsa", "pipewire-pulse", "pipewire-jack", "wireplumber", "pavucontrol",
     "cliphist", "wl-clipboard", "copyq", "git",
